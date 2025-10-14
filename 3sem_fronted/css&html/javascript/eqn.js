83b80704904286da65098c17e6c10a599fad4bf2
jahnavihanumantharaj <jahnavihanumantharaj1510@gmail.com>
2025-10-14 19:55:10 +0530

    day1-5

---
+++ 3sem_fronted/css&html/javascript/eqn.js
@@ -0,0 +1,7 @@
+let a=10;
+let b=13;
+let c=15;
+let d=20;
+
+console.log("your lucky number is :" ,(a+b),(c-d),(a/d),(c*a))
+
---
+++ 3sem_fronted/css&html/javascript/java.js
@@ -0,0 +1,13 @@
+let a=13;
+let b=15;
+console.log(a+b);
+let c=13;
+let d=15;
+console.log(c-d);
+let e=13;
+let f=15;
+console.log(e*f);
+let g=13;
+let h=15;
+console.log(g/h);
+
